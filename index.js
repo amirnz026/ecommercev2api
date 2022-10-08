07ef7a2174d7bb5f03dee8bb5e90a35b6f2aa548
Amir Hosein Nazari <czhablog@gmail.com>
2022-10-08 20:25:12 +0400

    feat: connected to local database

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose
+  .connect("mongodb://localhost/mydb")
+  .then(() => console.log("DBConnection Successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(5000, () => {
   console.log("Backend server is running!");
 });
